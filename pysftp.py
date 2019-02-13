b77237c5cbcd9a4d96cc1c35c0d54ab7b4617331
Dylan Forwood <37117314+pyCity@users.noreply.github.com>
2019-02-13 08:10:46 -0600

    Update pysftp.py

--- pysftp.py
+++ pysftp.py
@@ -1,25 +1,29 @@
 import sys
 import pysftp
 
+"""Simple sftp file transfer tool"""
 
-if len(sys.argv) != 4:
-    print("Usage: python pySftp.py [get/put] [ip] /path/to/info.txt")
-    sys.exit()
+username = "dylan"
+private_key = "/home/dylan/.ssh/id_rsa"
 
 
 def sftp_get(host, file):
-    with pysftp.Connection(host, username="dylan", private_key="/home/dylan/.ssh/id_rsa") as sftp:
+    with pysftp.Connection(host, username=username, private_key=private_key) as sftp:
         print("Connection established to {}\nDownloading {}...".format(host, file))
         sftp.get(file)
 
 
 def sftp_put(host, file):
-    with pysftp.Connection(host, username="dylan", private_key="/home/dylan/.ssh/id_rsa") as sftp:
+    with pysftp.Connection(host, username=username, private_key=private_key) as sftp:
         print("Connection established to {}\nUploading {}...".format(host, file))
         sftp.put(file)
 
 
 if __name__ == "__main__":
+
+    if len(sys.argv) != 4:
+        print("Usage: python pySftp.py [get/put] [ip] /path/to/info.txt")
+        sys.exit()
 
     option = sys.argv[1]
     host = sys.argv[2]
